fb4d99eb43b438fc5b22e3c1001b833791f7ac19
Jaemin Lee <contact.jaemin@gmail.com>
2024-11-27 03:22:24 -0800

    Created test api call and webapp application.

--- app/misty_api/views.py
+++ app/misty_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def test(request):
+    if request.method == "GET":
+        return Response({"test": "success"})
+    if request.method == "POST":
+        print(request.data)
+        return Response({"test": "postsuccess"})
